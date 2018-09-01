f14cf5e7e95b4f36915388b9506ae903b14414d3
Ricardo Numa <koki_numa@hotmail.com>
2018-09-01 18:46:59 -0300

    Insert coverage project

---
+++ Android/Coverage/app/src/androidTest/java/com/example/ricardonuma/coverage/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.example.ricardonuma.coverage;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldUpdateTextAfterButtonClick() {
+        onView(ViewMatchers.withId(R.id.button)).perform(click());
+
+        onView(withId(R.id.text)).check(matches(withText("Hello World!")));
+    }
+
+    @Test
+    public void shouldHideAfterButtonClick() {
+        onView(ViewMatchers.withId(R.id.hide)).perform(click());
+
+        onView(ViewMatchers.withId(R.id.hide)).check(matches(not(isDisplayed())));
+    }
+}
